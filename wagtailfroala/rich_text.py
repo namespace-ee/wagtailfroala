e08070e99c45e8388590e7f197473a9a2532fe92
Tõnis Väin <tonis@namespace.ee>
2019-07-29 14:32:18 +0300

    black

--- wagtailfroala/rich_text.py
+++ wagtailfroala/rich_text.py
@@ -14,12 +14,10 @@ from wagtail.wagtailcore.rich_text import DbWhitelister, expand_db_html
 class FroalaRichTextArea(WidgetWithScript, widgets.Textarea):
     def __init__(self, **kwargs):
         try:
-            self.options = kwargs.pop('options')
+            self.options = kwargs.pop("options")
         except KeyError:
-            self.options = {
-                'key': settings.FROALA_LICENSE_KEY
-            }
-            self.options.update(getattr(settings, 'FROALA_OPTIONS', {}))
+            self.options = {"key": settings.FROALA_LICENSE_KEY}
+            self.options.update(getattr(settings, "FROALA_OPTIONS", {}))
 
         super(FroalaRichTextArea, self).__init__(**kwargs)
 
@@ -34,10 +32,7 @@ class FroalaRichTextArea(WidgetWithScript, widgets.Textarea):
         return super(FroalaRichTextArea, self).render(name, translated_value, attrs)
 
     def render_js_init(self, id_, name, value):
-        return "makeFroalaRichTextEditable({0}, {1});".format(
-            json.dumps(id_),
-            json.dumps(self.options)
-        )
+        return "makeFroalaRichTextEditable({0}, {1});".format(json.dumps(id_), json.dumps(self.options))
 
     def value_from_datadict(self, data, files, name):
         original_value = super(FroalaRichTextArea, self).value_from_datadict(data, files, name)
@@ -47,28 +42,24 @@ class FroalaRichTextArea(WidgetWithScript, widgets.Textarea):
 
     @property
     def media(self):
-        js = [
-            static('froala/vendor/js/froala_editor.pkgd.min.js'),
-        ]
+        js = [static("froala/vendor/js/froala_editor.pkgd.min.js")]
 
         css = [
-            static('froala/css/wagtailfroala.css'),
-            static('froala/vendor/css/froala_editor.pkgd.min.css'),
-            '//cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/codemirror.min.css',
-            '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css',
+            static("froala/css/wagtailfroala.css"),
+            static("froala/vendor/css/froala_editor.pkgd.min.css"),
+            "//cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/codemirror.min.css",
+            "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css",
         ]
 
-        if getattr(settings, 'FROALA_CODEMIRROR', True):
-            js.append('//cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/codemirror.min.js')
-            js.append('//cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/mode/xml/xml.min.js')
-            css.append('//cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/codemirror.min.css')
+        if getattr(settings, "FROALA_CODEMIRROR", True):
+            js.append("//cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/codemirror.min.js")
+            js.append("//cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/mode/xml/xml.min.js")
+            css.append("//cdnjs.cloudflare.com/ajax/libs/codemirror/5.3.0/codemirror.min.css")
 
         # Maintain the order of JavaScript files.
-        js.append(static('froala/js/froala.js'))
+        js.append(static("froala/js/froala.js"))
 
-        if getattr(settings, 'FROALA_FONT_AWESOME', True):
-            css.append('//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css')
+        if getattr(settings, "FROALA_FONT_AWESOME", True):
+            css.append("//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css")
 
-        return Media(js=js, css={
-            'all': css
-        })
+        return Media(js=js, css={"all": css})
