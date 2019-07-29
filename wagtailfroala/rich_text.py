c4ab346fcade89d735cb15695c7f23ec1be9220c
Tõnis Väin <tonis@namespace.ee>
2019-07-29 14:33:16 +0300

    support wagtail >= 2.0

--- wagtailfroala/rich_text.py
+++ wagtailfroala/rich_text.py
@@ -7,8 +7,15 @@ from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.forms import Media, widgets
 
 from wagtail.utils.widgets import WidgetWithScript
-from wagtail.wagtailadmin.edit_handlers import RichTextFieldPanel
-from wagtail.wagtailcore.rich_text import DbWhitelister, expand_db_html
+from wagtail import __version__ as WAGTAIL_VERSION
+
+if WAGTAIL_VERSION >= "2.0":
+    from wagtail.admin.edit_handlers import RichTextFieldPanel
+    from wagtail.core.rich_text import features
+    from wagtail.admin.rich_text.converters.editor_html import EditorHTMLConverter
+else:
+    from wagtail.wagtailadmin.edit_handlers import RichTextFieldPanel
+    from wagtail.wagtailcore.rich_text import DbWhitelister, expand_db_html
 
 
 class FroalaRichTextArea(WidgetWithScript, widgets.Textarea):
@@ -20,6 +27,14 @@ class FroalaRichTextArea(WidgetWithScript, widgets.Textarea):
             self.options.update(getattr(settings, "FROALA_OPTIONS", {}))
 
         super(FroalaRichTextArea, self).__init__(**kwargs)
+        self.features = kwargs.pop("features", None)
+
+        if WAGTAIL_VERSION >= "2.0":
+            if self.features is None:
+                self.features = features.get_default_features()
+                self.converter = EditorHTMLConverter()
+            else:
+                self.converter = EditorHTMLConverter(self.features)
 
     def get_panel(self):
         return RichTextFieldPanel
@@ -28,7 +43,10 @@ class FroalaRichTextArea(WidgetWithScript, widgets.Textarea):
         if value is None:
             translated_value = None
         else:
-            translated_value = expand_db_html(value, for_editor=True)
+            if WAGTAIL_VERSION >= "2.0":
+                translated_value = self.converter.from_database_format(value)
+            else:
+                translated_value = expand_db_html(value, for_editor=True)
         return super(FroalaRichTextArea, self).render(name, translated_value, attrs)
 
     def render_js_init(self, id_, name, value):
@@ -38,7 +56,10 @@ class FroalaRichTextArea(WidgetWithScript, widgets.Textarea):
         original_value = super(FroalaRichTextArea, self).value_from_datadict(data, files, name)
         if original_value is None:
             return None
-        return DbWhitelister.clean(original_value)
+        if WAGTAIL_VERSION >= "2.0":
+            return self.converter.to_database_format(original_value)
+        else:
+            return DbWhitelister.clean(original_value)
 
     @property
     def media(self):
